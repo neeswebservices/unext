5907027dbb1dda1b24f8f83e89f59124e354bba0
neeswebservices <techneesofficial@gmail.com>
2022-03-06 14:09:42 -0800

    deploy

---
+++ components/header/index.tsx
@@ -0,0 +1,9 @@
+import { container, text } from './style'
+
+const Header = () => (
+    <div css={container}>
+        <h1 css={text}>All about next Js 🐱‍👤🐱‍👤</h1>
+    </div>
+)
+
+export default Header
---
+++ components/header/style.ts
@@ -0,0 +1,14 @@
+import { css } from '@emotion/react'
+
+const container = css`
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 1rem;
+`
+const text = css`
+    color: white;
+`
+
+export { container, text }
